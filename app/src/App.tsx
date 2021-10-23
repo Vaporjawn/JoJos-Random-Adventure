5fab9fbb4ddf1276398a0252816d2381581eafc6
Victor Williams <vrwilliams719@gmail.com>
2021-10-23 05:51:32 -0400

    added header and global css changes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import './global.css';
+import HomePage from './homepage/homepage';
 
 function App() {
   return (
     <div className="App">
-
+      <HomePage></HomePage>
     </div>
   );
 }
---
+++ app/src/homepage/homepage.tsx
@@ -0,0 +1,11 @@
+import '../global.css';
+
+export default function HomePage() {
+    return (
+      <div>
+        <h1> JoJo's Bizarre Adventure Randomizer </h1>
+  
+      </div>
+    );
+  }
+  
