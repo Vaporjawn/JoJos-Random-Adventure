941037e24d2c402cac691d7babb15d77bd55241d
Victor Williams <vrwilliams719@gmail.com>
2022-07-21 15:37:48 -0400

    VPR-256

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "./global.css";
+import HomePage from "./homepage/homepage";
+
+function App() {
+	return (
+		<div className="App">
+			<HomePage></HomePage>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/homepage/homepage.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+import "../global.css";
+import React from "react";
+
+export default function HomePage() {
+	return (
+		<div>
+			<h1> JoJo's Bizarre Adventure Randomizer </h1>
+  
+		</div>
+	);
+}
+  
