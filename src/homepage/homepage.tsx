88f8598889067a2b33da9415901236d73ca17b94
Victor Williams <vrwilliams719@gmail.com>
2022-08-08 12:54:30 -0400

    VPR-316

--- src/homepage/homepage.tsx
+++ src/homepage/homepage.tsx
@@ -3,6 +3,11 @@ import "../global.css";
 import React from "react";
 import Footer from "../components/footer/footer";
 
+const reload = () => {
+	window.location.reload();
+};
+
+
 const HomePage = () => {
 	return (
 		<div>
@@ -10,7 +15,7 @@ const HomePage = () => {
 				<img src="https://upload.wikimedia.org/wikipedia/commons/a/af/JoJo%27s_Bizarre_Adventure_-_Stardust_Crusaders_logo.png" alt="logo" />
 			</a>
 			{/* Need to make this H1 link to home, don't know if I have routes set up yet */}
-			<h1> JoJo's Bizarre Adventure Randomizer </h1>
+			<h1 onClick={() => reload()} > JoJo's Bizarre Adventure Randomizer </h1>
 			<Footer />
 		</div>
 	);
